afd5f9f989b59bd865ac45dec2f02d25e43d356a
Miphos <45264292+Miphos@users.noreply.github.com>
2021-11-12 19:01:08 +0300

    Delete Методы Стек вызов directory

---
+++ Работа с файловой подсистемой/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Работа_с_файловой_подсистемой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("D:\\startup.txt", Console.ReadLine());
+            File.AppendAllText("D:\\startup.txt","\n" + DateTime.Now.ToString());
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(new FileStream("space.bin", FileMode.OpenOrCreate), Console.ReadLine());
+
+        }
+    }
+}
